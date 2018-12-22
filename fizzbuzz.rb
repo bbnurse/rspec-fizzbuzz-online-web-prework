38b346b68a9de16eaaebdc782d8f772f081567b4
blanca bell <b.p.bell15@gmail.com>
2018-12-22 23:45:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,21 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require 'rpsec'
+
 
 
 def fizzbuzz(int)
-  return "Fizz" if fizz_3(int) = fizzbuzz(3)
-  return "Bizz" if fizz_5(int) = fizzbuzz(5)
-  return "FizzBuzz" if fizz15(int) = fizzbuzz(15)
-  int
-end
   if int % 3 == 0
-    "Fizz"
-  end
-  if int % 5 == 0
-     "Buzz"
-  end
-  if int % 15 == 0
-    "FizzBuzz"
-  end
+    int = "Fizz"
+  
+  elsif int % 5 == 0
+     int = "Buzz"
+
+  elsif int % 15 == 0
+    int = "FizzBuzz"
+
+  else
+    puts "not divisible by 3 or 5"
 end
