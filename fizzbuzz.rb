f6380d3e925e367922bf66c8916ab9172737c9c9
blanca bell <b.p.bell15@gmail.com>
2018-12-22 23:42:44 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,21 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require 'rpsec'
+
+
+def fizzbuzz(int)
+  return "Fizz" if fizz_3(int) = fizzbuzz(3)
+  return "Bizz" if fizz_5(int) = fizzbuzz(5)
+  return "FizzBuzz" if fizz15(int) = fizzbuzz(15)
+  int
+end
+  if int % 3 == 0
+    "Fizz"
+  end
+  if int % 5 == 0
+     "Buzz"
+  end
+  if int % 15 == 0
+    "FizzBuzz"
+  end
+end
